ce90ef0beac9e5aa082ce40c3f3f58461eb5aed7
Daniel Avzaradel <daniela@cgen.com>
2022-03-22 10:33:15 +0200

    unpack collections

--- python_ex001.py
+++ python_ex001.py
@@ -1,2 +1,42 @@
 hello = 'Hello World'
 print(hello)
+
+## PYTHON VARIABLES
+
+## variables
+x = 5
+y = 10
+print(x + y)
+
+a = str(3)
+b = int(3)
+c = float(3)
+print(a, b, c)
+
+## variable names
+
+myvar = 'Daniel'
+my_var = 'John'
+_my_var = 'Charlie'
+myVar = 'Goku'
+
+
+## Assign Multiple Values
+## many values to multiple variables
+
+fruita, fruitb, fruitc = 'Orange', 'Apple', 'Banana'
+print(fruita, fruitb, fruitc)
+
+## one value to multiple variables
+
+d = e = f = 'Bananas'
+print(d)
+print(e)
+print(f)
+
+
+## Unpack a collection
+
+fruits = ['Bananas', 'Cherries', 'Mangos']
+bananas, cherries, mangos = fruits
+print(bananas, cherries, mangos)
